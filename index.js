84d4258e8f20f817d88ebfb1028968a99b3edbd2
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-01-22 19:59:20 +0000

    Separated the prompt and model

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// Build and deploy gen AI applications on Google Cloud with Genkit and Node.js
-// https://codelabs.developers.google.com/codelabs/deploy-from-github/genkit-nodejs#6
-
-
 //To handle HTTP requests
 import express from 'express';
 const app = express();
 
 import { genkit } from 'genkit';
-import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+import { vertexAI } from '@genkit-ai/vertexai';
+
 
 const ai = genkit({
     plugins: [
@@ -17,11 +14,8 @@ const ai = genkit({
 
 app.get('/', async (req, res) => {
     const animal = req.query.animal || 'cat';
-    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
-    const llmResponse = await ai.generate({
-        model: gemini15Flash,
-        prompt: prompt,
-    });
+    const animalPrompt = ai.prompt('animal-facts');
+    const llmResponse = await animalPrompt({animal});
     const html = llmResponse.text;
     res.send(html);
 });
