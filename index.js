118c287dff66f3b6440e85f4aa65452e0f873fba
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-01-22 19:30:10 +0000

    Updated index js

--- index.js
+++ index.js
@@ -5,14 +5,29 @@
 
 //To handle HTTP requests
 import express from 'express';
-
-
 const app = express();
+
+import { genkit } from 'genkit';
+import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+
+const ai = genkit({
+    plugins: [
+        vertexAI({ location: 'us-central1' }),
+    ],
+});
+
 app.get('/', async (req, res) => {
-    res.send('HelloWorld homepath - from index.js');
+    const animal = req.query.animal || 'dog';
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+    const llmResponse = await ai.generate({
+        model: gemini15Flash,
+        prompt: prompt,
+    });
+    const html = llmResponse.text;
+    res.send(html);
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`codelab-genai: Listening on port ${port}`);
+    console.log(`codelab-genai: listening on port ${port}`);
 });
