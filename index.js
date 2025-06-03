d7d3130b511c5f737dbd01ddbd855ead37fa4571
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-06-03 00:42:57 +0000

    testing orig code

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
-
 const app = express();
+
+import { genkit } from 'genkit';
+import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+
+const ai = genkit({
+    plugins: [
+        vertexAI({ location: 'us-central1' }),
+    ],
+});
+
 app.get('/', async (req, res) => {
-    res.send('HelloWorld. mmmmm dbg!');
+    const animal = req.query.animal || 'camel';
+    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
+    const llmResponse = await ai.generate({
+        model: gemini15Flash,
+        prompt: prompt,
+    });
+    const html = llmResponse.text;
+    res.send(html);
 });
 
 const port = process.env.PORT || 8080;
