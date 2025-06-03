c5845145dc825a0be3d35b2bb86339c12cd1c85d
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-06-03 04:59:13 +0000

    Melbourne

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 
 import { genkit } from 'genkit';
 //import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';    //Internal Server Error
-import { gemini20Flash, vertexAI } from '@genkit-ai/vertexai';
-//import { vertexAI } from '@genkit-ai/vertexai';
+//import { gemini20Flash, vertexAI } from '@genkit-ai/vertexai';
+import { vertexAI } from '@genkit-ai/vertexai';
 
 const ai = genkit({
     plugins: [
@@ -16,10 +16,10 @@ app.get('/', async (req, res) => {
     const animal = req.query.animal || 'caterpillar';
     const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
     const llmResponse = await ai.generate({
-        model: gemini20Flash,
-        //model: vertexAI.model('gemini-2.0-flash'),
-        prompt: prompt,
-        //prompt: 'What should I do when I visit Melbourne?',
+        //model: gemini20Flash,
+        model: vertexAI.model('gemini-2.0-flash'),
+        //prompt: prompt,
+        prompt: 'What should I do when I visit Melbourne?',
     });
     const html = llmResponse.text;
     res.send(html);
