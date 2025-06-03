9249444bf565eae072a3609a4b98714ec79d67aa
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-06-03 03:23:15 +0000

    tst genkit

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
-import express from 'express';
-const app = express();
-
-import { genkit } from 'genkit';
-//import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
 import { vertexAI } from '@genkit-ai/vertexai';
 
 const ai = genkit({
-    plugins: [
-        vertexAI({ location: 'us-central1' }),
-    ],
+  plugins: [vertexAI({ location: 'us-central1' })],
 });
 
-app.get('/', async (req, res) => {
-    const animal = req.query.animal || 'camel';
-    const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
-    const llmResponse = await ai.generate({
-        //model: gemini15Flash,
-        model: vertexAI.model('gemini-2.0-flash'),
-        //prompt: prompt,
-        prompt: 'What should I do when I visit Melbourne?',
-    });
-    const html = llmResponse.text;
-    res.send(html);
+const llmResponse = await ai.generate({
+  model: vertexAI.model('gemini-2.0-flash'),
+  prompt: 'What should I do when I visit Melbourne?',
 });
-
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`codelab-genai: listening on port ${port}`);
-});
+console.log(llmResponse);
