17105c2785edaf3c05e83d6abcad229db54f2acb
EO @ CloudShellEditor <eloiisaorozco@gmail.com>
2025-06-03 04:51:09 +0000

    caterpillar 20

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 const app = express();
 
 import { genkit } from 'genkit';
-import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';
+//import { gemini15Flash, vertexAI } from '@genkit-ai/vertexai';    //Internal Server Error
+import { gemini20Flash, vertexAI } from '@genkit-ai/vertexai';
 //import { vertexAI } from '@genkit-ai/vertexai';
 
 const ai = genkit({
@@ -15,7 +16,7 @@ app.get('/', async (req, res) => {
     const animal = req.query.animal || 'caterpillar';
     const prompt = `Give me 10 fun facts about ${animal}. Return this as html without backticks.`
     const llmResponse = await ai.generate({
-        model: gemini15Flash,
+        model: gemini20Flash,
         //model: vertexAI.model('gemini-2.0-flash'),
         prompt: prompt,
         //prompt: 'What should I do when I visit Melbourne?',
